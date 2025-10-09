3a3a000f5e4bdbe200e90efd1cc19b80bb8b4746
amaliestok <astokland@hotmail.com>
2025-10-09 15:02:47 +0200

    Update funksjon_3.py

--- funksjon_3.py
+++ funksjon_3.py
@@ -4,4 +4,4 @@
 
 def skriv_ut_liste(emnekode_liste, semester_liste, studiepoeng_liste):
     for i in range(len(emnekode_liste)):
-        print(f"Emnekoden = {emnekode_liste[i]:.2f}, Studiepoeng = {studiepoeng_liste[i]:.2f}, semester_liste = {semester_liste[i]:.2f}")
+        print(f"Emnekoden = {emnekode_liste[i]}, Studiepoeng = {studiepoeng_liste[i]}, semester_liste = {semester_liste[i]}")
