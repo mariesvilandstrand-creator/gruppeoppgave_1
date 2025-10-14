68dba93c8bdc2fa62a7005e00b5756a7f31f5b96
amaliestok <astokland@hotmail.com>
2025-10-14 13:00:07 +0200

    hh

--- oving_7_aleksander.py
+++ oving_7_aleksander.py
@@ -1,7 +1,9 @@
 import csv
 
+
+
 with open("studieplan_fil.csv", "r") as fila:
-    csvreader = csv.reader(fila) 
+    csvreader = csv.reader(fila)
     
     for row in csvreader:
-        print(row)
+        print(row[1])
