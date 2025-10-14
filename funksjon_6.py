5896b6c2b080171f7bd396f61f79900fa70c85d5
amaliestok <astokland@hotmail.com>
2025-10-14 14:19:58 +0200

    hh

--- funksjon_6.py
+++ funksjon_6.py
@@ -2,6 +2,6 @@
 #informasjonen, det er opp til dere å finne ut hvilket format denne fila skal ha
 
 def lagre_emne_studieplan_fil():
-    print("...")
-    
-with open ("studieplan_fil.csv", "a", encoding="UTF-8") as fila:
+    with open ("studieplan_fil.csv", "a", encoding="UTF-8") as fila:
+        for i in len(studieplan_liste):
+            fila.write(f"{studieplan_liste[i]}, {emnekode_liste[i]}, {studiepoeng_liste[i]}")
