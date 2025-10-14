bc91e8c8c16c17a2fad195c5b98f7345c0cef5a8
amaliestok <astokland@hotmail.com>
2025-10-14 13:17:06 +0200

    hh

--- funksjon_7.py
+++ funksjon_7.py
@@ -1,2 +1,14 @@
 #Les inn emnene og studieplanen fra fil. Denne skal lese samme fil som bel skrevet i
 #punkt 6.
+
+import csv
+
+def lese_fil():
+    with open("studeplan_fil.csv", "r") as fila:
+        csvreader = csv.reader(fila)
+    for row in csvreader:
+        if not row:
+            continue
+        if row[0].startswith[row+1]:
+            print(f"semesteret ditt er: {row[0]}, emnekoden din er {row[1]}, studiepoengene er {row[2]}")
+            
--- oving_7_aleksander.py
+++ oving_7_aleksander.py
@@ -6,4 +6,6 @@ with open("studieplan_fil.csv", "r") as fila:
     csvreader = csv.reader(fila)
     
     for row in csvreader:
-        print(row[1])
+        if not row:
+            continue
+        print(row)
