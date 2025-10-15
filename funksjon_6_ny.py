d3f8827dc21271f223561b8a9caae4aa8e1b08d8
mariesvilandstrand-creator <mariesvilandstrand@gmail.com>
2025-10-15 16:25:58 +0200

    r

--- funksjon_6_ny.py
+++ funksjon_6_ny.py
@@ -1,9 +1,9 @@
 import csv
 
 def lagre_emne_studieplan_fil(studieplan_liste, studiepoeng_sum):
-    with open ("studieplan_fil.csv", "a", encoding="UTF-8") as fila:
+    with open ("studieplan_fil.csv", "a", newline="", encoding="UTF-8") as fila:
         skriver=csv.writer(fila)
-        for i in range(2):
+        for i in range(len(studieplan_liste)):
             fila.write(f"{(studieplan_liste[i][0])},{(studiepoeng_sum[i][0])}\n")
             fila.write(f"{(studieplan_liste[i][1])},{(studiepoeng_sum[i][1])}\n")
             fila.write(f"{(studieplan_liste[i][2])},{(studiepoeng_sum[i][2])}\n")
