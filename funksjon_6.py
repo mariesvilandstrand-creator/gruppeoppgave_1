45085270edd87efd9277a47acfa5a1dc5f2d08e0
amaliestok <astokland@hotmail.com>
2025-10-15 10:05:39 +0200

    hh

--- funksjon_6.py
+++ funksjon_6.py
@@ -2,25 +2,13 @@
 #informasjonen, det er opp til dere å finne ut hvilket format denne fila skal ha
 import csv
 
-
-
 def lagre_emne_studieplan_fil(studieplan_liste, studiepoeng_liste):
     with open ("studieplan_fil.csv", "a", encoding="UTF-8") as fila:
-        for i in len(studieplan_liste[i]):
-            
-            fila.write(f"1, {studieplan_liste[i][0]}, {studiepoeng_liste[i][0]}")
-
-        for i in len(studieplan_liste[i]):
-            fila.write(f"2, {studieplan_liste[i][1]}, {studiepoeng_liste[i][1]}")       
-                    
-        for i in len(studieplan_liste[i]):
-            fila.write(f"3, {studieplan_liste[i][2]}, {studiepoeng_liste[i][2]}")
-
-        for i in len(studieplan_liste[i]):
-            fila.write(f"4, {studieplan_liste[i][3]}, {studiepoeng_liste[i][3]}")
-
-        for i in len(studieplan_liste[i]):
-            fila.write(f"5, {studieplan_liste[i][4]}, {studiepoeng_liste[i][4]}")
-
-        for i in len(studieplan_liste[i]):
-            fila.write(f"6, {studieplan_liste[i][5]}, {studiepoeng_liste[i][5]}")
+        skriver=csv.writer(fila)
+        for i in range(len(studieplan_liste)):
+            fila.write(f"1, {studieplan_liste[i][0]}, {studiepoeng_liste[i][0]}\n")
+            fila.write(f"2, {studieplan_liste[i][1]}, {studiepoeng_liste[i][1]}\n")       
+            fila.write(f"3, {studieplan_liste[i][2]}, {studiepoeng_liste[i][2]}\n")
+            fila.write(f"4, {studieplan_liste[i][3]}, {studiepoeng_liste[i][3]}\n")
+            fila.write(f"5, {studieplan_liste[i][4]}, {studiepoeng_liste[i][4]}\n")
+            fila.write(f"6, {studieplan_liste[i][5]}, {studiepoeng_liste[i][5]}\n")
--- test_12.py
+++ test_12.py
@@ -1,1 +1,23 @@
-print("Hei jeg heter amalie\n og jeg er 20 år")
+liste_1=[1,3,5]
+liste_2=[2,4,6]
+liste_3=[7,8,9]
+full_liste=[liste_1,liste_2,liste_3]
+liste_ok_1=["lol","hei","hei"]
+full_liste_ok=[liste_ok_1,liste_ok_1,liste_ok_1]
+
+import csv
+with open("test_1.csv", "w", encoding="UTF-8") as fila:
+    skriver=csv.writer(fila)
+    for i in range(len(full_liste)):
+        fila.write(f"{str(full_liste[i][0])}\n")
+        fila.write(f"{str(full_liste_ok[i][0])}\n")
+        fila.write(f"{str(full_liste[i][1])}\n")
+        fila.write(f"{str(full_liste_ok[i][1])}\n")
+        fila.write(f"{str(full_liste[i][2])}\n")
+        fila.write(f"{str(full_liste_ok[i][2])}\n")
+    
+
+
+
+    
+    
