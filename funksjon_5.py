d984dd3dab3438a13430956044a88a6bf4218cdd
Aleksanderkk99 <aleksanderkk76@gmail.com>
2025-10-15 09:28:49 +0200

    Update funksjon_5.py

--- funksjon_5.py
+++ funksjon_5.py
@@ -3,15 +3,5 @@
 #hvilke semestre som ikke er gyldige og hvor mange studiepoeng emner det er i det
 #semesteret.
 
-from funksjon_4 import skriv_ut_studieplan
 
 def sjekk_studieplan_gyldig():
-    gyldig = True
-    for i, sem in enumerate(skriv_ut_studieplan, start=1):
-        total_sp = sum(e["studiepoeng"] for e in sem)
-        if total_sp != 30:
-            print(f"Semester {i} har {total_sp} studiepoeng (skal være 30).")
-            gyldig = False
-    if gyldig:
-        print("Studieplanen er gyldig")
-
