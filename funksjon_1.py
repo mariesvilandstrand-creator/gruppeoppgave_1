9ed56faefba61cbdccf93d13fe0ae3f2bbae0a31
amaliestok <astokland@hotmail.com>
2025-10-15 09:24:15 +0200

    Update funksjon_1.py

--- funksjon_1.py
+++ funksjon_1.py
@@ -10,14 +10,13 @@ def legg_til_nytt_emne(emnekode_liste, semester_liste, studiepoeng_liste, aarsti
     emnekode=input("Hva er emnekoden?")
     
     while len(emnekode) > 6 and len(emnekode)<6:
-        emnekode=input("\nHva er emnekoden?")
-        ()
+        emnekode=input("Hva er emnekoden?")
     emnekode_liste.append(emnekode)
-    semester=int(input("\nHvilket semester er det?"))
+    semester=int(input("Hvilket semester er det?"))
     
     while semester>6 or semester<1:
         print("FEIL")
-        semester=int(input("\nHvilket semester er det?"))
+        semester=int(input("Hvilket semester er det?"))
     semester_liste.append(semester)
     
     
