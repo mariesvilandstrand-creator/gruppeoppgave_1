d889cb32faa753fe6ca2a0959480aab1e41ea4af
amaliestok <astokland@hotmail.com>
2025-10-15 17:26:24 +0200

    ENDELIG FERDIG!!!!!!

--- funksjon_6_ny.py
+++ funksjon_6_ny.py
@@ -1,12 +1,8 @@
-import csv
+
 
 def lagre_emne_studieplan_fil(studieplan_liste, studiepoeng_sum):
-    with open ("studieplan_fil.csv", "a", newline="", encoding="UTF-8") as fila:
-        skriver=csv.writer(fila)
-        for i in range(len(studieplan_liste)):
-            fila.write(f"{(studieplan_liste[i][0])},{(studiepoeng_sum[i][0])}\n")
-            fila.write(f"{(studieplan_liste[i][1])},{(studiepoeng_sum[i][1])}\n")
-            fila.write(f"{(studieplan_liste[i][2])},{(studiepoeng_sum[i][2])}\n")
-            fila.write(f"{(studieplan_liste[i][3])},{(studiepoeng_sum[i][3])}\n")
-            fila.write(f"{(studieplan_liste[i][4])},{(studiepoeng_sum[i][4])}\n")
-            fila.write(f"{(studieplan_liste[i][5])},{(studiepoeng_sum[i][5])}\n")
+    with open ("studieplan_fil.txt", "w", newline="", encoding="UTF-8") as fila:
+        for i, semester in enumerate(studieplan_liste):
+            fila.write(f"Semester, {i+1}, Antall studiepoeng: , {sum(studiepoeng_sum[i])}\n")
+            for j, emne in enumerate(semester):
+                fila.write(f"{emne}, studiepoeng: {studiepoeng_sum[i][j]} \n")
