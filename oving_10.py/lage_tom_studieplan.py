52ebe42e9d3710021e4c49adb036ef4d05a35bb8
amaliestok <astokland@hotmail.com>
2025-11-12 16:38:45 +0100

    hh

---
+++ oving_10.py/lage_tom_studieplan.py
@@ -0,0 +1,10 @@
+
+from klasse import*
+
+def lage_tom_studieplan():
+    
+    student_id = int(input("Hva er student ID'en din?"))
+    
+    linje_navn = input("Hvilken linje går du på?")
+    
+    Studieplan(student_id, linje_navn)
